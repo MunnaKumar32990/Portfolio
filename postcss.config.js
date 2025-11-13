b2e7da834457567d4bd0414301d5e7fdb81d7f02
MunnaKumar32990 <2300032990@kluniversity.in>
2025-11-13 15:38:52 +0530

    fix merger conflict issues

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
-=======
-module.exports = {
-  plugins: [
-    'tailwindcss',
-    'autoprefixer',
-  ],
-} 
->>>>>>> 9fd5605f5bed21736c6dc54d10531329d2e9d166
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
@@ -22,59 +21,3 @@ export default {
   },
   plugins: [],
 };
-=======
-module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      animation: {
-        'meteor': 'meteor 5s linear infinite',
-        'shimmer': 'shimmer 2s linear infinite',
-        'spotlight': 'spotlight 2s ease .75s 1 forwards',
-        'scroll': 'scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite',
-      },
-      keyframes: {
-        meteor: {
-          '0%': { transform: 'rotate(215deg) translateX(0)', opacity: '1' },
-          '70%': { opacity: '1' },
-          '100%': {
-            transform: 'rotate(215deg) translateX(-500px)',
-            opacity: '0',
-          },
-        },
-        shimmer: {
-          '0%': {
-            backgroundPosition: '0 0',
-          },
-          '100%': {
-            backgroundPosition: '-200% 0',
-          },
-        },
-        spotlight: {
-          '0%': {
-            opacity: '0',
-            transform: 'translate(-72%, -62%) scale(0.5)',
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'translate(-50%,-40%) scale(1)',
-          },
-        },
-        scroll: {
-          to: {
-            transform: 'translate(calc(-50% - 0.5rem))',
-          },
-        },
-      },
-    },
-  },
-  darkMode: 'class',
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-  ],
-} 
->>>>>>> 9fd5605f5bed21736c6dc54d10531329d2e9d166
